8977e0dfb69d199b3b2c499975ed40c745d7186a
Mison1212 <misonwenda20@gmail.com>
2025-11-07 09:13:53 +0700

    membuat spalsh_screen1

---
+++ lib/screen/splash_screen1.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 50),
+
+            Container(
+              width: 250,
+              height: 250,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blueGrey,
+                image: DecorationImage(
+                  image: AssetImage("")
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
